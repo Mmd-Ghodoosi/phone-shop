fab048f185df53902af05613f7b9ca8de089994f
mmd-ghodoosi <mamadakgh@gmail.com>
2024-06-21 01:11:58 +0330

    using tailwind in Products section

--- components/sections/Products.tsx
+++ components/sections/Products.tsx
@@ -27,82 +27,94 @@ const Products = () => {
   }, []);
 
   return (
-    <fieldset
-      style={{
-        marginTop: 300,
-        textAlign: "center",
-        borderRight: "none",
-        borderLeft: "none",
-        borderBottom: "none",
-      }}
-    >
+    <fieldset className="mt-80 text-center border-t-2 border-t-black">
       <legend>محصولات فروشگاه</legend>
-      <Grid container>
-        {products.map((product) => (
-          <Grid
-            xs={12}
-            sm={6}
-            md={6}
-            lg={4}
-            xl={4}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            mt={10}
-            key={product._id}
-          >
-            <Card
-              sx={{
-                maxWidth: 345,
-                width: "100%",
-                height: 350,
-              }}
-            >
-              <CardMedia>
-                <Link
-                  href={`/products/${product._id}`}
-                  target="_blank"
-                  style={{ textDecoration: "none", color: "#4b4747" }}
-                >
-                  <Image
-                    src={require(`../../assets/iphone/${product.picture}`)}
-                    alt={product.name}
-                  />
-                </Link>
-              </CardMedia>
 
-              <CardContent sx={{ textAlign: "center" }}>
-                <Typography
-                  variant="h5"
-                  component="div"
-                  textAlign={"right"}
-                  pt={2}
-                >
-                  <Link
-                    href={`/products/${product._id}`}
-                    target="_blank"
-                    style={{ textDecoration: "none", color: "#4b4747" }}
-                  >
-                    {product.name}
-                  </Link>
-                </Typography>
-                <Typography
-                  variant="body2"
-                  textAlign={"left"}
-                  pt={4}
-                  fontWeight={"bold"}
-                >
-                  قیمت{"   "}
-                  {product.price}
-                  {"   "}تومان
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
+      <section className=" grid grid-cols-1 gap-4 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1">
+        {products.map((product) => (
+          <div className="border-2 border-black rounded-xl ">
+            <div className=" flex justify-center items-center mt-12   text-center">
+              <Link
+                href={`/products/${product._id}`}
+                target="_blank"
+                className="text-[#4b4747]  hover:scale-105  duration-200"
+              >
+                <Image
+                  src={require(`../../assets/iphone/${product.picture}`)}
+                  alt={product.name}
+                  className="w-64 h-64 "
+                />
+              </Link>
+            </div>
+            <div className="pt-4">
+              <Link
+                href={`/products/${product._id}`}
+                target="_blank"
+                className="text-[#4b4747] text-xl hover:text-blue-500 hover:underline"
+              >
+                {product.name}
+              </Link>
+            </div>
+            <div className="pt-2 font-medium">
+              قیمت{"   "}
+              {product.price}
+              {"   "}تومان
+            </div>
+          </div>
         ))}
-        <Grid xs={0} sm={0} md={0} lg={0} xl={0}></Grid>
-      </Grid>
+      </section>
     </fieldset>
+    //<Grid
+    //       xs={12}
+    //       sm={6}
+    //       md={6}
+    //       lg={4}
+    //       xl={4}
+    //       display={"flex"}
+    //       justifyContent={"center"}
+    //       alignItems={"center"}
+    //       mt={10}
+    //       key={product._id}
+    //     >
+    //       <Card
+    //         sx={{
+    //           maxWidth: 345,
+    //           width: "100%",
+    //           height: 350,
+    //         }}
+    //       >
+    //         <CardMedia>
+    //
+    //         </CardMedia>
+
+    //         <CardContent sx={{ textAlign: "center" }}>
+    //           <Typography
+    //             variant="h5"
+    //             component="div"
+    //             textAlign={"right"}
+    //             pt={2}
+    //           >
+    //             <Link
+    //               href={`/products/${product._id}`}
+    //               target="_blank"
+    //               style={{ textDecoration: "none", color: "#4b4747" }}
+    //             >
+    //               {product.name}
+    //             </Link>
+    //           </Typography>
+    //           <Typography
+    //             variant="body2"
+    //             textAlign={"left"}
+    //             pt={4}
+    //             fontWeight={"bold"}
+    //           >
+    //             قیمت{"   "}
+    //             {product.price}
+    //             {"   "}تومان
+    //           </Typography>
+    //         </CardContent>
+    //       </Card>
+    //     </Grid>
   );
 };
 
