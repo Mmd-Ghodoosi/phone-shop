deed342ed6950c00d656ff0e79d9b6d061da3c08
mmd-ghodoosi <mamadakgh@gmail.com>
2024-07-20 20:10:19 +0330

    added dropdown menu to navbar section

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -1,17 +1,38 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
+import Cookies from "js-cookie";
+
 import { FaSearch, FaPhoneAlt } from "react-icons/fa";
+import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
+import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 
 const Navbar = () => {
+  const [token, setToken] = useState<string>("");
+  const [menu, setMenu] = useState<boolean>(false);
+
+  
+  useEffect(() => {
+    const session = Cookies.get("session");
+    session ? setToken(session) : null;
+  }, []);
+
+  const handleLogout = () => {
+    Cookies.remove("session");
+    window.location.reload();
+  };
+
   return (
     <section className="flex justify-between items-center bg-gray-400 w-full h-12">
+      {/* logo section */}
       <div className="pl-2 text-xl">
         <Link href="/" className="text-black ">
           <FaPhoneAlt />{" "}
         </Link>
       </div>
+      {/* search section */}
       <div className="flex mt-2 ">
         <input
           type="text"
@@ -22,11 +43,74 @@ const Navbar = () => {
         />
         <FaSearch className="cursor-pointer" />
       </div>
-      <div className="pr-2   ">
-        <Link href={`/profile/id`}>
-          <CgProfile className="text-4xl" />
-        </Link>
-      </div>
+      {/* dropdown section */}
+      {token === process.env.NEXT_PUBLIC_TOKEN ? (
+        <div className="pr-2">
+          <button
+            className=" flex items-center"
+            onClick={() => setMenu((prev) => !prev)}
+          >
+            {menu ? (
+              <MdOutlineKeyboardDoubleArrowUp />
+            ) : (
+              <MdOutlineKeyboardDoubleArrowDown />
+            )}
+            <CgProfile className="text-4xl cursor-pointer" />
+            {/* menu section */}
+            {menu ? (
+              <div className="z-10  bg-white divide-y text-center divide-gray-100 rounded-lg shadow w-30 h-36 absolute top-14 right-0  dark:bg-gray-700 dark:divide-gray-600">
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                  {/* profile link inside dropdown */}
+                  <li>
+                    <a
+                      href="/profile/:id"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      پروفایل
+                    </a>
+                  </li>
+                  {/* cart link inside dropdown*/}
+                  <li>
+                    <a
+                      href="/cart"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      سبد خرید
+                    </a>
+                  </li>
+                </ul>
+                {/* logout button */}
+                <div className="py-2">
+                  <div
+                    className="block px-4 py-2 text-sm cursor-pointer text-red-700 hover:bg-red-100 dark:hover:bg-red-600 dark:text-gray-200 dark:hover:text-white"
+                    onClick={handleLogout}
+                  >
+                    خروج
+                  </div>
+                </div>
+              </div>
+            ) : null}
+          </button>
+        </div>
+      ) : (
+        <div>
+          {/* signup && signin section */}
+          <Link
+            href={"/auth/signup"}
+            className="text-gray-800 hover:text-blue-200"
+          >
+            ثبت نام
+          </Link>
+          {"     "}/ {"     "}
+          <Link
+            href={"/auth/signin"}
+            className="text-gray-800 hover:text-blue-200"
+          >
+            {" "}
+            ورود
+          </Link>
+        </div>
+      )}
     </section>
   );
 };
